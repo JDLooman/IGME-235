1c2ff51e2a068e2368968ea110f39c45e156b79f
Justin Looman (RIT Student) <jdl9046@rit.edu>
2021-11-10 14:16:18 -0500

    added some more ffunctionality now a name is shown and you can random search

--- project2/js/myscripts.js
+++ project2/js/myscripts.js
@@ -1,11 +1,12 @@
-window.onload = (e) => {document.querySelector("#search").onclick = searchButtonClicked};
-
-window.onload = (e) => {document.querySelector("#random").onclick = searchRandomClicked};
+window.onload = (e) => {
+    document.querySelector("#search").onclick = searchButtonClicked;
+    document.querySelector("#random").onclick = randomSearch;
+};
 
 let diplayterm = "";
 
 function searchButtonClicked(){
-    const GIPHY_URL = "https://pokeapi.co/api/v2/pokemon/";
+    const GIPHY_URL = "https://pokeapi.co/api/v2/pokemon-form/";
 
     let url = GIPHY_URL;
 
@@ -27,12 +28,12 @@ function searchButtonClicked(){
     getData(url);
 }
 
-fucntion searchRandomClicked(){
-    const GIPHY_URL = "https://pokeapi.co/api/v2/pokemon/";
+function randomSearch(){
+    const GIPHY_URL = "https://pokeapi.co/api/v2/pokemon-form/";
 
     let url = GIPHY_URL;
 
-    let term = getRandomInt()
+    let term = getRandomInt(200);
 
     url += term;
 
@@ -52,6 +53,40 @@ function getData(url){
     xhr.send();
 }
 
-fucntion dataLoaded(e){
+function dataLoaded(e){
     let xhr = e.target;
+
+    console.log(xhr.responseText);
+
+    let result = JSON.parse(xhr.responseText);
+
+    document.querySelector("#content").innerHTML = "Showing result for '" + result.name.toUpperCase() + "' " + result.type;
+}
+
+function dataError(e){
+    console.log("An error occured");
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+  
+  // Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
 }
